f62647adc545cb03703ae53afe93b639d424fb97
BigPigHoof <285961971@qq.com>
2020-04-30 12:24:35 +0800

    去掉hash

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,37 @@
 
 module.exports = {
-    // ... 
-    publicPath:'./',
-    assetsDir: "assets",
-    productionSourceMap: false,
-    configureWebpack: {
-        externals: {
-            'AMap': 'AMap', // 高德地图配置           
-            'AMapUI': 'AMapUI'
-          },
-      
-      },
-    //   proxyTable: { 
-    //     '/api': {  //使用"/api"来代替"http://f.apiplus.c" 
-    //       target: 'http://api.m.taobao.com', //源地址 
-    //       changeOrigin: true, //改变源 
-    //       pathRewrite: { 
-    //         '^/api': 'http://api.m.taobao.com' //路径重写 
-    //         } 
-    //     } 
-    //   },
-      devServer: {
-        proxy: {
-          '/taobaoapi': {
-            target: 'http://api.m.taobao.com',
-            changeOrigin: true,
-          },
+  // ... 
+  publicPath: './',
+  assetsDir: "assets",
+  productionSourceMap: false,
+  filenameHashing:false,
+  configureWebpack: {
+    externals: {
+      'AMap': 'AMap', // 高德地图配置           
+      'AMapUI': 'AMapUI'
+    },
 
-        }
-      }
-      
+  },
+  //   proxyTable: { 
+  //     '/api': {  //使用"/api"来代替"http://f.apiplus.c" 
+  //       target: 'http://api.m.taobao.com', //源地址 
+  //       changeOrigin: true, //改变源 
+  //       pathRewrite: { 
+  //         '^/api': 'http://api.m.taobao.com' //路径重写 
+  //         } 
+  //     } 
+  //   },
+  devServer: {
+    proxy: {
+      '/taobaoapi': {
+        target: 'http://api.m.taobao.com',
+        changeOrigin: true,
+      },
+
+    }
+  },
+
+
+
 
 }
